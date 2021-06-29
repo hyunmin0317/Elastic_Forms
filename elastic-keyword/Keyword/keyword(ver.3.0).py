f907c58e596c60356075cb7c6661d8fc13f7f169
hyunmin0317 <choihm9903@naver.com>
2021-06-29 15:30:08 +0900

    Update Crawling

--- elastic-keyword/Keyword/keyword(ver.3.0).py
+++ elastic-keyword/Keyword/keyword(ver.3.0).py
@@ -1,42 +1,50 @@
+import csv
 import pandas as pd
-import sys
-from krwordrank.word import KRWordRank
-from krwordrank.hangle import normalize
-import krwordrank
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+from krwordrank.word import summarize_with_keywords
 
-fname = 'data.csv'
-csv = pd.read_csv(fname, names=['texts', 'scores'], encoding='UTF-8')
+keyword_list = []
+data = pd.read_csv('data.csv', names=['word', 'score'], encoding='UTF-8')
 
-texts_data = csv['texts']
+texts_data = data['word']
 texts_val = texts_data.values
 texts = texts_val.tolist()
 
-scores_data = csv['scores']
-scores_val = scores_data.values
-scores = scores_val.tolist()
+# stopwords : 키워드에서 제거될 단어
+stopwords = {'자료:', '10', '20', '그림', '따른', '수는', '경우', '-1', '주:', '것으로', '대한'}
 
+keywords = summarize_with_keywords(texts, min_count=5, max_length=10,
+    beta=0.85, max_iter=10, stopwords=stopwords)
 
-wordrank_extractor = KRWordRank(
-    min_count = 5, # 단어의 최소 출현 빈도수 (그래프 생성 시)
-    max_length = 10, # 단어의 최대 길이
-    verbose = True
-    )
+#print(keywords)
 
-beta = 0.85    # PageRank의 decaying factor beta
-max_iter = 10
+for word, score in sorted(keywords.items(), key=lambda x:-x[1])[:50]:
+    print('%8s:\t%.4f' % (word, score))
+    keyword_list.append([word, score])
 
-keywords, rank, graph = wordrank_extractor.extract(texts, beta, max_iter)
-
-for word, r in sorted(keywords.items(), key=lambda x:x[1], reverse=True)[:50]:
-    print('%8s:\t%.4f' % (word, r))
-
-import csv
-keyword_list = []
-
-for word, r in sorted(keywords.items(), key=lambda x:x[1], reverse=True)[:50]:
-    keyword_list.append([word,r])
-    
-with open('keyword.csv','w', newline='', encoding='utf-8-sig') as f:
+with open('keyword2.csv','w', newline='', encoding='utf-8-sig') as f:
     makewrite = csv.writer(f)
     for value in keyword_list:
         makewrite.writerow(value)
+
+# Set your font path
+font_path = './NanumFont/NanumBarunGothic.ttf'
+
+krwordrank_cloud = WordCloud(
+    font_path = font_path,
+    width = 800,
+    height = 800,
+    background_color="white"
+)
+
+krwordrank_cloud = krwordrank_cloud.generate_from_frequencies(keywords)
+fig = plt.figure(figsize=(10, 10))
+plt.imshow(krwordrank_cloud, interpolation="bilinear")
+plt.axis('off')
+plt.show()
+fig.savefig('./keyword2.png')
+
+
+
+
