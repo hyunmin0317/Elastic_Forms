694636c6ea15a8d984b882306e2e2d39a2738bcb
hyunmin0317 <choihm9903@naver.com>
2021-07-19 17:13:01 +0900

    Update covid19

--- covid19/covid19-kr.py
+++ covid19/covid19-kr.py
@@ -1,4 +1,15 @@
-import pandas as pd
+from urllib import request
+import wget
+from bs4 import BeautifulSoup
+import urllib.request
 
-covid19 = pd.read_html('https://github.com/jooeungen/coronaboard_kr/blob/master/kr_daily.csv', header=0, encoding='utf-8')
-print(covid19)
+
+
+URL = 'https://github.com/jooeungen/coronaboard_kr/blob/master/kr_daily.csv'
+
+source_code_from_URL = urllib.request.urlopen(URL)
+soup = BeautifulSoup(source_code_from_URL, 'lxml', from_encoding='utf-8')
+
+for tr in soup.find_all('tr', class_='js-file-line'):
+    td = tr.select('td')
+    print(td)
